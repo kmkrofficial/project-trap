a186cf490583a0a4d994e5f382382788b22d6c4b
Keerthi Raajan K M <kmkrworks@gmail.com>
2024-10-29 13:35:07 +0530

    fundamental_data scrape

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+from scrape import get_fundamental_stock_data, get_ohlc_data
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    ohlc_data = get_ohlc_data("ICICIBANK.NS")
+    fundamental_data = get_fundamental_stock_data("ICICIBANK.NS")
+    print(ohlc_data)
+    print(fundamental_data)
+    return render_template("index.html", ohlc_data=ohlc_data, fundamental_data=fundamental_data)
 
 if __name__ == "__main__": 
     app.run(port=8080)
--- scrape.py
+++ scrape.py
@@ -1,16 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
 
-def get_ohlc_data(stock_name):
+def make_request(path):
     headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"}
-    response = requests.get(f"https://finance.yahoo.com/quote/{stock_name}/history/", headers=headers)
+    response = requests.get(f"https://finance.yahoo.com{path}", headers=headers)
     if response.status_code != 200:
-        print("Error has occurred")
+        print("MakeRequest : Error has occurred")
+        return None
+    return BeautifulSoup(response.content, 'html5lib')
+
+def get_ohlc_data(stock_name):
+    parsed = make_request(f"/quote/{stock_name}/history/")
+    if parsed == None:
         return {
             "status" : "failure",
             "data" : "Unable to retrieve"
         }
-    parsed = BeautifulSoup(response.content, 'html5lib')
     table = parsed.find("table")
     theaders = table.find("thead")
     tbody = table.find("tbody")
@@ -28,4 +33,24 @@ def get_ohlc_data(stock_name):
         "data" : ohlc_data
     }
 
-print(get_ohlc_data("DJT"))
+def get_fundamental_stock_data(stock_name):
+    parsed = make_request(f"/quote/{stock_name}/key-statistics/")
+    if parsed == None:
+        return {
+            "status" : "failure",
+            "data" : "Unable to retrieve"
+        }
+    financial_info = {}
+    financial_info_section = parsed.find("div", {"class" : "column yf-14j5zka"})
+    
+    for item in financial_info_section.find_all("table"):
+        for row in item.find_all("tr"):
+            key = row.find("td", {"class" : "label"}).find(string=True).strip()
+            value = row.find("td", {"class" : "value"}, string=True).find(string=True).strip()
+            financial_info[key] = value
+    
+    return financial_info
+
+# print(get_fundamental_stock_data("DJT"))
+
+# print(get_ohlc_data("DJT"))
